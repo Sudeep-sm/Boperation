fb2c1fb2791d9f7aa60bc061d2a3d6d83df69d42
Sudeep-sm <noobadum00@gmail.com>
2025-05-07 14:59:06 +0530

    save

---
+++ BasicOperation.java
@@ -0,0 +1,12 @@
+public class BasicOperation{
+    public static void main(String[] args){
+        int a=10;
+        int b=5; 
+        System.out.println("The addition result is:"+(a+b));
+        System.out.println("The subtraction result is:"+(a-b));
+        System.out.println("The multiplication result is:"+(a*b));
+        System.out.println("The division result is:"+(a/b));
+        
+    }
+
+}
